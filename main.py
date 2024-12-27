0c65c2efee022419df4aa9b4880ad0f290e517e0
unknown <dar5114@mail.ru>
2024-12-27 15:08:40 +0300

    значительное улучшение функционала

--- main.py
+++ main.py
@@ -1,20 +1,103 @@
-import asyncio
-import random
-from telebot.async_telebot import AsyncTeleBot
+import telebot
+from utils.parsing_structures import params_dict, directions_dict, params_list, names_dict
 from config import TOKEN
+from utils.table_loader import response
 
-bot = AsyncTeleBot(TOKEN)
+bot = telebot.TeleBot(TOKEN)
+
+global_direction_param = ''
+global_direction = ''
+global_plan_param = ''
+global_plan = ''
 
 
 @bot.message_handler(commands=['start'])
-async def send_welcome(message):
-    text = 'Добро пожаловать! С вами Бот-ассистент Магистрант.'
-    await bot.reply_to(message, text)
+def send_welcome(message):
+    chat_id = message.chat.id
+    text = 'Добро пожаловать! С вами Бот-ассистент Магистрант.\nЯ показываю информацию по дисциплинам ИВМИИТ.\n' \
+           'Выберите направление'
+    keyboard = telebot.types.InlineKeyboardMarkup()
+    for dir in params_dict.keys():
+        sdir = str(dir)
+        button = telebot.types.InlineKeyboardButton(text=sdir, callback_data=str(directions_dict[sdir]))
+        keyboard.add(button)
+    bot.send_message(chat_id, text, reply_markup=keyboard)
+
+
+@bot.callback_query_handler(func=lambda call: call.data in [str(val) for val in directions_dict.values()])
+def choose_plan(call):
+    global global_direction_param
+    global_direction_param = call.data
+    message = call.message
+    chat_id = message.chat.id
+
+    dict = None
+    for key in directions_dict.keys():
+        if str(directions_dict[key]) == call.data:
+            dict = params_dict[key]
+            global global_direction
+            global_direction = str(key)
+
+    text = global_direction + '\nТеперь выберите учебный план'
+
+    keyboard = telebot.types.InlineKeyboardMarkup()
+    for dir in dict.keys():
+        sdir = str(dir)
+        button = telebot.types.InlineKeyboardButton(text=sdir, callback_data=str(dict[sdir]))
+        keyboard.add(button)
+
+    bot.send_message(chat_id, text, reply_markup=keyboard)
+
+
+@bot.callback_query_handler(func=lambda call: call.data in params_list)
+def choose_course(call):
+    global global_plan_param
+    global_plan_param = call.data
+    message = call.message
+    chat_id = message.chat.id
+
+    for key in params_dict[global_direction]:
+        if call.data == params_dict[global_direction][key]:
+            global global_plan
+            global_plan = str(key)
+
+    text = global_direction + '\n' + global_plan + '\nТеперь выберите курс'
+    keyboard = telebot.types.InlineKeyboardMarkup()
+    if names_dict[global_direction] == 1:
+        button1 = telebot.types.InlineKeyboardButton(text='1', callback_data='1')
+        button2 = telebot.types.InlineKeyboardButton(text='2', callback_data='2')
+        button3 = telebot.types.InlineKeyboardButton(text='3', callback_data='3')
+        button4 = telebot.types.InlineKeyboardButton(text='4', callback_data='4')
+        keyboard.add(button1, button2, button3, button4)
+    if names_dict[global_direction] == 0:
+        button1 = telebot.types.InlineKeyboardButton(text='1', callback_data='1')
+        button2 = telebot.types.InlineKeyboardButton(text='2', callback_data='2')
+        keyboard.add(button1, button2)
+
+    bot.send_message(chat_id, text, reply_markup=keyboard)
+
+
+@bot.callback_query_handler(func=lambda call: call.data in ['1', '2', '3', '4'])
+def send_data_message(call):
+    course = call.data
+    message = call.message
+    chat_id = message.chat.id
+    text = response(global_direction_param, global_plan_param, course)
+    bot.send_message(chat_id, str(text))
+
+
+@bot.message_handler(commands=['close'])
+def close(message):
+    chat_id = message.chat.id
+    bot.send_message(chat_id, 'Сессия завершена!')
+    bot.stop_bot()
 
 
 @bot.message_handler(func=lambda message: True)
-async def echo_message(message):
+def unknown_message(message):
     text = 'Я не знаю, что на это ответить!'
-    await bot.reply_to(message, text)
+    bot.reply_to(message, text)
 
-asyncio.run(bot.polling())
+
+if __name__ == '__main__':
+    bot.infinity_polling()
---
+++ utils/table_loader.py
@@ -0,0 +1,146 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+import pandas as pd
+
+
+def prepare_table(html_table):
+    df = pd.read_html("<table>" + html_table + "/table")[0]
+    df = df.drop(index=[0, 1, 2, 3])
+    df = df.drop(df.columns[[2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 14, 15, 16, 19]], axis=1)
+    df.columns = ['Discp', 'Name', 'Exam1', 'Test1', 'Exam2', 'Test2']
+    disciplines = df[['Discp', 'Name', 'Exam1', 'Test1', 'Exam2', 'Test2']].values.tolist()
+    names = df[['Discp', 'Name']].values.tolist()
+
+    return disciplines, names
+
+
+def parse_dv(names):
+    dv_list = []
+    count = 1
+    for el in names:
+        st = str(el[0]).split('.')
+        if 'ДВ' not in st or len(st) < 5:
+            continue
+        dv = []
+        if 'ДВ' in st and len(st) == 5:
+            dv.append(count)
+            dv.append(el[1])
+            names.remove(el)
+            for el2 in names:
+                st2 = str(el2[0]).split('.')
+                if 'ДВ' not in st2 or len(st2) < 5:
+                    continue
+                if st2[3] == st[3]:
+                    dv.append(el2[1])
+                    names.remove(el2)
+            dv_list.append(dv)
+        count = count + 1
+
+    return dv_list
+
+
+def fill_disciples(disciplines, dv_list):
+    sem1_test = []
+    sem1_exam = []
+    sem2_test = []
+    sem2_exam = []
+    for el in disciplines:
+        st = str(el[0]).split('.')
+        if 'ДВ' in st:
+            continue
+        if el[2] == '+':
+            sem1_exam.append(el[1])
+        if el[3] == '+':
+            sem1_test.append(el[1])
+        if el[4] == '+':
+            sem2_exam.append(el[1])
+        if el[5] == '+':
+            sem2_test.append(el[1])
+    symb = '●️'
+    for dv in dv_list:
+        cur_dv = f'Блок дисциплин по выбору {dv[0]}'
+        fl = True
+        for el in disciplines:
+            if el[1] in dv:
+                if el[2] == '+':
+                    if fl:
+                        sem1_exam.append(cur_dv)
+                        fl = False
+                    sem1_exam.append(symb + el[1])
+                if el[3] == '+':
+                    if fl:
+                        sem1_test.append(cur_dv)
+                        fl = False
+                    sem1_test.append(symb + el[1])
+                if el[4] == '+':
+                    if fl:
+                        sem2_exam.append(cur_dv)
+                        fl = False
+                    sem2_exam.append(symb + el[1])
+                if el[5] == '+':
+                    if fl:
+                        sem2_test.append(cur_dv)
+                        fl = False
+                    sem2_test.append(symb + el[1])
+
+    return sem1_exam, sem1_test, sem2_exam, sem2_test
+
+
+def response_text(sem1_exam, sem1_test, sem2_exam, sem2_test, course):
+    course = int(course)
+    text = f'Семестр {2*course-1} \n\nЗачеты:\n'
+    for s in sem1_test:
+        text = text + s + '\n'
+    text = text + f'\nЭкзамены:\n'
+    for s in sem1_exam:
+        text = text + s + '\n'
+    text = text + f'\nСеместр {2*course}\n\nЗачеты:\n'
+    for s in sem2_test:
+        text = text + s + '\n'
+    text = text + f'\nЭкзамены:\n'
+    for s in sem2_exam:
+        text = text + s + '\n'
+
+    return text
+
+
+def form_processing(direction, plan, course):
+    options = Options()
+    options.add_argument('--headless=new')
+    driver = webdriver.Chrome(options=options)
+    driver.get("https://shelly.kpfu.ru/e-ksu/study_plan_for_web?P_FACULTY=9&p_portal=1")
+
+    select_element = driver.find_element(By.XPATH, "//*[@id='dim_content2']/div[2]/table/tbody/tr/td[2]/select")
+    select = Select(select_element)
+    select.select_by_value(direction)
+
+    select_element = driver.find_element(By.XPATH, "//*[@id='dim_content2']/div[2]/table/tbody/tr[2]/td[2]/select")
+    select = Select(select_element)
+    select.select_by_value(plan)
+
+    select_element = driver.find_element(By.XPATH,
+                                         "// *[ @ id = 'dim_content2'] / div[2] / table / tbody / tr[3] / td[2] / select")
+    select = Select(select_element)
+    select.select_by_value(course)
+
+    driver.find_element(By.XPATH, "//*[@id='submit_ask']/input").click()
+
+    table = driver.find_element(By.CLASS_NAME, 'T_TABLE')
+    html_table = table.get_attribute('innerHTML')
+    driver.quit()
+
+    return html_table
+
+
+def response(direction, plan, course):
+    html_table = form_processing(direction, plan, course)
+
+    disciplines, names = prepare_table(html_table)
+
+    dv_list = parse_dv(names)
+
+    sem1_exam, sem1_test, sem2_exam, sem2_test = fill_disciples(disciplines, dv_list)
+
+    return response_text(sem1_exam, sem1_test, sem2_exam, sem2_test, course)
